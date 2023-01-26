e4c92dc53a1d0949372e80c88d27faecac748cc7
NanatiMengistu <120086986+NanatiMengistu@users.noreply.github.com>
2023-01-26 17:08:42 +0300

    Add files via upload

---
+++ Encapsulationn.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+public class Account{
+    private long acc_no;
+ private string name,email;
+ private folate amount;
+ public long getAcc_no () {
+  return acc_no;
+     
+    }
+ public void setAcc_no(long acc_no) {
+  this.acc_no = acc_no;
+  }
+  public void String getName() {
+ 
+   return name;
+}
+public void setName (string name){
+ this.name = name;
+ 
+ }
+ public string getEmail(string email){
+  this.email = email;
+  }
+}
+}
